7ebc8c517dbab746c8121950c8f943d4aed7f24a
peter <peterw1114@gmail.com>
2019-12-11 23:57:43 +0100

    revert iou calculations and fix row localizing

--- results.py
+++ results.py
@@ -8,16 +8,12 @@ from PIL import Image
 from scipy.ndimage import label, generate_binary_structure
 
 TRAIN_PATH = './severstal-steel-defect-detection/train_images/'
-TEST_PATH = './severstal-steel-defect-detection/test_images/'
 
 # load dataframe with train labels
 train_df = pd.read_csv('./severstal-steel-defect-detection/train.csv')
 
 # load my predictions
-results_df = pd.read_csv('./submission_training.csv')
-
-print(train_df.head(3))
-print(results_df.head(3))
+results_df = pd.read_csv('./submission_many_iterations.csv')
 
 
 def preprocess_df(df):
@@ -33,16 +29,11 @@ def preprocess_df(df):
     new_df['Image'] = split_df[0]
     new_df['Label'] = split_df[1]
 
-    # check the result
     return new_df
-
 
 
 train_df = preprocess_df(train_df)
 results_df = preprocess_df(results_df)
-
-print(train_df.columns.values.tolist())
-print(train_df.head(1).values.tolist())
 
 
 def get_row_by_img_clsid(df, fname):
@@ -74,27 +65,27 @@ def rle2maskResize(rle):
 
     return mask.reshape((height, width), order='F')
 
-def compute_iou(y_true, y_pred):
-    a = y_true.tolist()
-    return jaccard_score(y_true.tolist(), y_pred.tolist(), average='micro')
+# def compute_iou(y_true, y_pred):
+#     a = y_true.tolist()
+#     return jaccard_score(y_true.tolist(), y_pred.tolist(), average='micro')
 
 
-# def compute_iou(target, prediction):
-#     '''
-#     Function to compute IOU metric
-#     See:
-#     https://www.jeremyjordan.me/evaluating-image-segmentation-models/
-#     '''
-#     target = np.asarray(target, dtype=float)
-#     prediction = np.asarray(prediction, dtype=float)
-#
-#     smooth = 1e-5  # smoothing for empty masks
-#
-#     intersection = np.logical_and(target, prediction)
-#     union = np.logical_or(target, prediction)
-#     iou_score = np.sum(intersection) / (np.sum(union) + smooth)
-#
-#     return iou_score
+def compute_iou(target, prediction):
+    '''
+    Function to compute IOU metric
+    See:
+    https://www.jeremyjordan.me/evaluating-image-segmentation-models/
+    '''
+    target = np.asarray(target, dtype=float)
+    prediction = np.asarray(prediction, dtype=float)
+
+    smooth = 1e-5  # smoothing for empty masks
+
+    intersection = np.logical_and(target, prediction)
+    union = np.logical_or(target, prediction)
+    iou_score = np.sum(intersection) / (np.sum(union) + smooth)
+
+    return iou_score
 
 
 
@@ -158,13 +149,15 @@ def plot_mask_by_id(idx):
     '''
     Plots true mask and predicted mask by id in train_df
     '''
-    image_name = train_df.iloc[idx]['Image']
+
+    pred_mask = results_df.iloc[idx]['EncodedPixels']
+    img_name_defect = results_df.iloc[idx]["ImageId_ClassId"]
+    rle_mask = get_mask_by_img_clsid(train_df, img_name_defect)
+    image_name = get_row_by_img_clsid(train_df, img_name_defect).iloc[0]['Image']
+
+
     image_filename = TRAIN_PATH + image_name
     image = Image.open(image_filename)
-
-    rle_mask = train_df.iloc[idx]['EncodedPixels']
-    img_name = train_df.iloc[idx]["ImageId_ClassId"]
-    pred_mask = get_mask_by_img_clsid(results_df, img_name)
 
     defect = train_df.iloc[idx]['Label']
 
@@ -185,74 +178,16 @@ def plot_mask_by_id(idx):
     axs[1].axis('off')
     axs[1].set_title('Predicted mask for defect #{}'.format(defect))
 
-    plt.suptitle('IOU for image: {:.2f}'.format(iou), fontsize=16)
+    plt.suptitle('IOU for image: {:.8f}'.format(iou), fontsize=16)
 
     plt.show()
 
 
-def get_mask(line_id):
-    '''
-    Function to visualize the image and the mask.
-    INPUT:
-        line_id - id of the line to visualize the masks
-    RETURNS:
-        np_mask - numpy segmentation map
-    '''
-
-    # convert rle to mask
-    rle = train_df.loc[line_id]['EncodedPixels']
-
-    np_mask = rle2maskResize(rle)
-    np_mask = np.clip(np_mask, 0, 1)
-
-    return np_mask
-
-
-def add_mask_areas(train_df):
-    '''
-    Helper function to add mask area as a new column to the dataframe
-    INPUT:
-        train_df - dataset with training labels
-    '''
-    masks_df = train_df.copy()
-    masks_df['Area'] = 0
-
-    for i, row in masks_df.iterrows():
-        masks_df['Area'].loc[i] = np.sum(get_mask(i))
-
-    return masks_df
-
-
-def add_mask_number(train_df):
-    '''
-    Helper function to add mask area as a new column to the dataframe
-    INPUT:
-        train_df - dataset with training labels
-    '''
-    masks_df = train_df.copy()
-    masks_df['NumMasks'] = 0
-
-    s = generate_binary_structure(2, 2)
-
-    for i, row in masks_df.iterrows():
-        mask = get_mask(i)
-
-        if np.sum(mask) > 0:
-            labeled_array, labels = label(mask, structure=s)
-            masks_df['NumMasks'].loc[i] = labels
-        else:
-            masks_df['NumMasks'].loc[i] = 0
-
-    return masks_df
-
-# masks_df = add_mask_number(train_df)
-
-
 def get_iou_by_id(idx):
+    pred_mask = results_df.iloc[idx]['EncodedPixels']
+    img_name = results_df.iloc[idx]["ImageId_ClassId"]
+    rle_mask = get_mask_by_img_clsid(train_df, img_name)
 
-    rle_mask = train_df.iloc[idx]['EncodedPixels']
-    img_name = train_df.iloc[idx]["ImageId_ClassId"]
-    pred_mask = get_mask_by_img_clsid(results_df, img_name)
 
     true = rle2maskResize(rle_mask)
     pred = rle2maskResize(pred_mask)
@@ -261,44 +196,24 @@ def get_iou_by_id(idx):
     return iou
 
 
-train_img_ids = train_df.index.values
+results_img_ids = results_df.index.values
 
 
 def has_mask(row):
     return isinstance(row['EncodedPixels'], str)
 
 
-def calculate_avarage_iou(train_img_ids):
+def calculate_avarage_iou(results_img_ids):
     total_iou = 0
-    for idx in train_img_ids:
-        if has_mask(train_df.iloc[idx]):
-            total_iou += get_iou_by_id(idx)
-    average_iou = total_iou/len(train_img_ids)
+    ids = results_img_ids[:10]
+    for idx in ids:
+        total_iou += get_iou_by_id(idx)
+    average_iou = total_iou/len(ids)
     return average_iou
 
-# print("Average IOU:")
-# print(calculate_avarage_iou(train_img_ids))
+print("Average IOU: {:.8f}".format(calculate_avarage_iou(results_img_ids)))
+
+# for idx in results_img_ids[:10]:
+#     plot_mask_by_id(idx)
 
 
-for idx in train_img_ids[30:40]:
-    plot_mask_by_id(idx)
-
-#
-#
-# many_masks_df = masks_df[masks_df['NumMasks'] > 10]
-# line_ids = many_masks_df.index.values
-# #
-# #
-# # rnd_idx = line_ids[np.random.randint(len(line_ids))]
-# # plot_mask_by_id(rnd_idx)
-# #
-# # rnd_idx = line_ids[np.random.randint(len(line_ids))]
-# # plot_mask_by_id(rnd_idx)
-# #
-# #
-# #
-# #
-# total_iou = sum([get_iou_by_id(iid) for iid in line_ids])
-# average_iou = total_iou/len(line_ids)
-# print("Average IOU:")
-# print(average_iou)
